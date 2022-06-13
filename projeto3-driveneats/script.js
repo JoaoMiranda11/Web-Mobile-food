a093369df4ce4aa9a7db216505e07e100f95715e
João Miranda <jsm2.pe@gmail.com>
2022-06-13 16:58:12 -0300

    Final

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,17 +1,22 @@
+var pedidos = {}
+
 function addProduto(produto, tipo) {
     let el = document.getElementById(produto);
     if (el.classList.contains('selected')) {
         el.classList.remove('selected');
+        delete pedidos[tipo]
     } else {
         let campo = document.getElementById(tipo);
         Object.entries(campo.children).forEach((e)=>{
-            e[1].classList.remove('selected')
+            e[1].classList.remove('selected');
         });
+        pedidos[tipo] = {
+            nome: el.children[1].attributes.value.nodeValue,
+            valor: el.children[3].attributes.value.nodeValue,
+        }
         el.classList.add('selected');
     }
-
-    acionarBotao( verificarMarcacoes() );
-
+    acionarBotao(verificarMarcacoes());
 }
 
 
@@ -39,10 +44,53 @@ function acionarBotao(value) {
     }
 }
 
-function fecharPedido() {
+function fecharPedido(act) {
     let botao = document.getElementById('botaoFechar');
-    if (botao.classList.contains('ok')) {
-        document.getElementById('modbg').classList.add('appears');
-        document.getElementById('modcontent').classList.add('appears');
+    if (act){
+        if (botao.classList.contains('ok')) {
+
+            let total = (
+                parseFloat(pedidos.campo1.valor) +
+                parseFloat(pedidos.campo2.valor) +
+                parseFloat(pedidos.campo3.valor)
+            ).toFixed(2)
+
+            document.getElementById('compras').innerHTML = `
+                <div class="compras">
+                    <div class="lista">${pedidos.campo1.nome}</div>
+                    <div class="listaValor">${pedidos.campo1.valor}</div>
+                </div>
+                <div class="compras">
+                    <div class="lista">${pedidos.campo2.nome}</div>
+                    <div class="listaValor">${pedidos.campo2.valor}</div>
+                </div>
+                <div class="compras">
+                    <div class="lista">${pedidos.campo3.nome}</div>
+                    <div class="listaValor">${pedidos.campo3.valor}</div>
+                </div>
+                <div class="compras">
+                    <div class="lista total">Total</div>
+                    <div class="listaValor total">R$ ${total}</div>
+                </div>
+            `;
+            document.getElementById('modbg').classList.add('appears');
+            document.getElementById('modcontent').classList.add('appears');
+        }
+    } else {
+        document.getElementById('modbg').classList.remove('appears');
+        document.getElementById('modcontent').classList.remove('appears');
     }
+}
+
+function pedir() {
+    document.getElementById('modbg').classList.remove('appears');
+    document.getElementById('modcontent').classList.remove('appears');
+
+    campos = ['campo1', 'campo2', 'campo3'];
+    campos.forEach((e)=>{
+        let campo = document.getElementById(e);
+        let verify = Object.entries(campo.children).map((e) => (
+            e[1].classList.remove('selected')
+        ));
+    });
 }
